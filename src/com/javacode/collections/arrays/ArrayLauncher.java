05c6c21aee20b9f0d340d78a4d4f139a2a0cb530
Dima <free-wind-2012@yandex.ru>
2018-12-15 18:53:55 +0300

    Lesson 40. Сортировка одномерного массива

--- src/com/javacode/collections/arrays/ArrayLauncher.java
+++ src/com/javacode/collections/arrays/ArrayLauncher.java
@@ -8,21 +8,37 @@ public class ArrayLauncher {
     public static void main(String[] args) {
         int[] myArray = new int[8];
         System.out.println("Please enter 8 int elements");
+
         for (int j = 0; j < myArray.length; j++) {
             System.out.println("Next element");
             myArray[j] = scanner.nextInt();
         }
-
-        for (int i = 0; i < myArray.length; i++) {
+        int[] array = sort(myArray);
+        for (int i = 0; i < array.length; i++) {
             System.out.println("Element # " + i + " = " + myArray[i]);
         }
-/*        myArray[0] = 0;
-        myArray[1] = 100;
-        myArray[2] = 200;
-        myArray[3] = 300;
-        myArray[4] = 400;
-        myArray[5] = 500;
-        myArray[9] = 900;*/
     }
 
+    private static int[] sort(int[] array) {
+//        int[] myNewArray = Arrays.copyOf(array, array.length); //копирование одного массива в другой
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        return array;
+    }
+
+/*
+        10, 5, 16, 4
+        5, 10, 16, 4
+        5, 10, 4, 16
+        5, 4, 10, 16
+        4, 5, 10, 16
+*/
 }
+
