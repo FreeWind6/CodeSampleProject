fdad819f33595a956cf4946195a30113c0d650dd
Dima <free-wind-2012@yandex.ru>
2019-01-12 14:32:25 +0300

    Lesson 57

---
+++ src/com/javacode/collections/set/Car.java
@@ -0,0 +1,57 @@
+package com.javacode.collections.set;
+
+public class Car {
+    private final String carBrand;
+    private final String model;
+    private final Integer pricePerDay;
+
+//    если они final то нужен конструктор, иначе ошибка компиляции
+
+    public Car(String carBrand, String model, int pricePerDay) {
+        this.carBrand = carBrand;
+        this.model = model;
+        this.pricePerDay = pricePerDay;
+    }
+
+    public String getCarBrand() {
+        return carBrand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getPricePerDay() {
+        return pricePerDay;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || this.getClass() != obj.getClass()) {
+            return false;
+        }
+        Car car = (Car) obj;
+        if (!this.carBrand.equals(car.getCarBrand())) {
+            return false;
+        }
+        if (!this.model.equals(car.getModel())) {
+            return false;
+        }
+        return this.pricePerDay.equals(car.getPricePerDay());
+    }
+
+//  x.equals(x) = true рефлексивность
+//  x.equals(y) = true y.equals(x) = true = симетрия
+//  a = b and b = c then a = c транзитивность
+    @Override
+    public int hashCode() {
+        int result = carBrand.hashCode();
+        result = 31 * result + model.hashCode();
+        result = 31 * result + pricePerDay.hashCode();
+        return result;
+    }
+}
--- src/com/javacode/collections/set/SetRunner.java
+++ src/com/javacode/collections/set/SetRunner.java
@@ -1,18 +1,16 @@
 package com.javacode.collections.set;
 
-import java.util.Arrays;
-import java.util.LinkedHashSet;
+import java.util.HashSet;
 import java.util.Set;
-import java.util.TreeSet;
 
 public class SetRunner {
     public static void main(String[] args) {
-/*        Set - хранит только уникальные эл-ты но не повторяет их при одинаковом названии
-*         TreeSet - сортирует эл-ты по алфавиту
-*         HashSet - рандомно выводит
-*         LinkedHashSet - выводит в том порядке что мы записали
-* */
-        String[] cars = {"Mini", "Mers", "Audi", "VM", "Smart", "Toyota", "Porsche"};
+        /*        Set - хранит только уникальные эл-ты но не повторяет их при одинаковом названии
+         *         TreeSet - сортирует эл-ты по алфавиту
+         *         HashSet - рандомно выводит
+         *         LinkedHashSet - выводит в том порядке что мы записали
+         * */
+/*        String[] cars = {"Mini", "Mers", "Audi", "VM", "Smart", "Toyota", "Porsche"};
         String[] otherCars = {"Audi", "Ford", "GMC", "Toyota", "Chevrolet"};
 
         Set<String> carSet = new TreeSet<>(Arrays.asList(cars));
@@ -22,10 +20,35 @@ public class SetRunner {
         Set<String> uniqueCars = new LinkedHashSet<>(carSet);
         uniqueCars.addAll(orherCarSet);
 
+        print(uniqueCars);*/
+
+        Set<Car> sixCars = new HashSet<>();
+        sixCars.add(new Car("VW", "Golf", 45));
+        sixCars.add(new Car("Audi", "A3", 60));
+        sixCars.add(new Car("VM", "Polo", 25));
+        sixCars.add(new Car("BMW", "Z4", 120));
+        sixCars.add(new Car("BMW", "440i", 200));
+
+        Set<Car> europaCars = new HashSet<>();
+        europaCars.add(new Car("Toyota", "Auris", 40));
+        europaCars.add(new Car("Reno", "Logan", 30));
+        europaCars.add(new Car("Reno", "Megan", 50));
+        europaCars.add(new Car("VW", "Golf", 45));
+        europaCars.add(new Car("VM", "Polo", 35));
+
+        Set<Car> uniqueCars = new HashSet<>(sixCars);
+        uniqueCars.addAll(europaCars);
         print(uniqueCars);
+
+
     }
 
-    private static void print(Set<String> cars) {
-        System.out.println(cars);
+    private static void print(Set<Car> cars) {
+        System.out.printf("%-20s %-20s %-20s \n", "Car brand", "Model", "Price per day");
+//        -20 это отступ
+//        s - это тип, стринговое значение
+        for (Car car : cars) {
+            System.out.printf("%-20s %-20s %-20s \n", car.getCarBrand(), car.getModel(), car.getPricePerDay());
+        }
     }
 }
