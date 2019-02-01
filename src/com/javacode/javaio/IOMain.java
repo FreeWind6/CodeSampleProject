ed6dc892aeed26d4b5af87ffcdca5b174e1489b2
Dima <free-wind-2012@yandex.ru>
2019-02-01 14:55:46 +0300

    Lesson 66

--- src/com/javacode/javaio/IOMain.java
+++ src/com/javacode/javaio/IOMain.java
@@ -4,31 +4,36 @@ import com.javacode.collections.map.treemap.AverageStudentGrade;
 import com.javacode.collections.map.treemap.SubjectGrade;
 import com.javacode.collections.map.treemap.TreeMapRunner;
 
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.NavigableMap;
 import java.util.Set;
 
 public class IOMain {
-    public static void main(String[] args) {
+
+    private static final String FILE_NAME = "GradeBook.txt";
+
+    public static void main(String[] args) throws IOException {
         NavigableMap<AverageStudentGrade, Set<SubjectGrade>> grades = TreeMapRunner.createGrades();
-        FileWriter writer = null;
-        try {
-            writer = new FileWriter("GradeBook.txt");
+        readFile(grades);
+        readFile();
+    }
+
+    private static void readFile() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME));
+        String c;
+        while ((c = reader.readLine()) != null) {
+            System.out.println(c);
+        }
+    }
+
+    private static void readFile(NavigableMap<AverageStudentGrade, Set<SubjectGrade>> grades) throws IOException {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(FILE_NAME))) {
             for (AverageStudentGrade gradeKey : grades.keySet()) {
                 writer.write("===========================\n");
                 writer.write("Student: " + gradeKey.getName() + " Average grade: " + gradeKey.getAverageGrade() + "\n");
                 for (SubjectGrade grade : grades.get(gradeKey)) {
                     writer.write("Subject: " + grade.getSubject() + " Grade: " + grade.getGrade() + "\n");
                 }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                writer.close();
-            } catch (IOException e) {
-                e.printStackTrace();
             }
         }
     }
