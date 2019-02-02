170d057087126e17f5735b33980ac48c27615409
Dima <free-wind-2012@yandex.ru>
2019-02-02 19:28:54 +0300

    Lesson 67

--- src/com/javacode/javaio/IOMain.java
+++ src/com/javacode/javaio/IOMain.java
@@ -5,8 +5,7 @@ import com.javacode.collections.map.treemap.SubjectGrade;
 import com.javacode.collections.map.treemap.TreeMapRunner;
 
 import java.io.*;
-import java.util.NavigableMap;
-import java.util.Set;
+import java.util.*;
 
 public class IOMain {
 
@@ -14,11 +13,35 @@ public class IOMain {
 
     public static void main(String[] args) throws IOException {
         NavigableMap<AverageStudentGrade, Set<SubjectGrade>> grades = TreeMapRunner.createGrades();
-        readFile(grades);
-        readFile();
+//        writeFile(grades);
+//        reedFile();
+
+/*        try (FileInputStream reader = new FileInputStream(FILE_NAME);
+             FileOutputStream writer = new FileOutputStream("GradeBookByte.txt")) {
+            int c;
+            while ((c = reader.read()) != -1) {
+                System.out.print(c);
+                writer.write(c);
+            }
+        }*/
+
+        Formatter formatter = new Formatter("BankAccounts.txt");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter clientID, clientName, client surname, account balance");
+        int i = 0;
+        while (i < 3) {
+            try {
+                formatter.format("%d, %s, %s, %.2f%n", scanner.nextInt(), scanner.next(), scanner.next(), scanner.nextFloat());
+                i++;
+            } catch (InputMismatchException e) {
+                System.out.println("Input is incorrect. Please try again");
+                scanner.nextLine();
+            }
+        }
+        formatter.close();
     }
 
-    private static void readFile() throws IOException {
+    private static void reedFile() throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME));
         String c;
         while ((c = reader.readLine()) != null) {
@@ -26,7 +49,7 @@ public class IOMain {
         }
     }
 
-    private static void readFile(NavigableMap<AverageStudentGrade, Set<SubjectGrade>> grades) throws IOException {
+    private static void writeFile(NavigableMap<AverageStudentGrade, Set<SubjectGrade>> grades) throws IOException {
         try (PrintWriter writer = new PrintWriter(new FileWriter(FILE_NAME))) {
             for (AverageStudentGrade gradeKey : grades.keySet()) {
                 writer.write("===========================\n");
